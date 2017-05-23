9b4292fd991f201888c44dfa51938afccecc1bc8
gaopiji <448030133@qq.com>
2017-05-23 10:20:03 +0800

    完成后端接口数据获取及渲染功能demo

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var path = require('path');
 
 var TOKEN = 'grzx';
 var app=express();
+var userId;
+var userInfo;
 	//定义EJS模板引擎和模板文件位置
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
@@ -41,33 +43,25 @@ function readAccessToken(){
 	return data;	
 }
 
-function get_req1(code){
+function get_userId(res,code){
 		//var post_str = new Buffer(JSON.stringify(reJSON));
-	var userId;
-		//设置get请求参数
-	var get_options={
-		host:'qyapi.weixin.qq.com',
-		port:'80',
-		path:'/cgi-bin/user/getuserinfo?access_token='+access_token+'&code='+code,
-		method:'GET',
-		headers:{
-			'Content-Type': 'application/x-www-form-urlencoded',
-		}
-	};
 	var get_req = https.request('https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token='+access_token+'&code='+code,function(response){
 		var responseText=[];
 		var size=0;
 		response.setEncoding('utf8');
 		response.on('data',function(data){
-			console.log(data);
+		//	console.log(data);
+			var j = JSON.parse(data)
 			responseText.push(data);
-			console.log(data.UserId);
-			if(data.UserId!=null)
-			 	userId = data.UserId;
-
+			if(j.UserId!=null)
+			 	userId = j.UserId;
+				console.log(userId);
 			});
 		response.on('end',function(){
-			console.log(responseText);
+			if(userId!=null){
+			get_userInfo(res,userId,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		}
+			//console.log(responseText);
 			//responseText.UserId
 		});
 	});
@@ -77,12 +71,53 @@ function get_req1(code){
 	get_req.end();
 }
 
+//获取学生信息接口数据
+function get_userInfo(res,userId,userInfoToken){
+		//https://api.mysspku.com/index.php/V2/StudentInfo/getDetail?stuid=STUID&token=TOKEN
+		//设置get请求参数
+		var get_options={
+		host:'api.mysspku.com',
+		path:'/index.php/V2/StudentInfo/getDetail?stuid='+userId+'&token='+userInfoToken,
+		method:'GET',
+		rejectUnauthorized: false,
+	};
+	//	console.log("https://api.mysspku.com/index.php/V2/StudentInfo/getDetail?stuid="+userId+"&token="+userInfoToken);
+	var get_req = https.request(get_options,function(response){
+		var responseText=[];
+		var size=0;
+		response.setEncoding('utf8');
+		response.on('data',function(data){
+			console.log(data);
+			responseText.push(data);
+			var j = JSON.parse(data);
+			if(j.errcode==40001){
+				userInfo=j.data;
+			}
+		});
+		response.on('end',function(){
+			renderSdudent(res,"学生信息",userInfo)
+			//console.log(responseText);
+		});
+	});
 
+	//对于前面设置请求和json数据进行发送
+	//get_req.write(post_str);
+	get_req.end();
+}
+
+//渲染学生信息页面
+function renderSdudent(res,titlep,userInfo){
+		console.log(userInfo);
+		res.render('studentInfo',{
+		title:titlep,
+		username:userInfo.name,
+		});
+}
 
 app.get('/',function(req,res){
 	var arg = url.parse(req.url).query;
 	var code = querystring.parse(arg).code;
-	get_req1(code);
+	get_userId(res,code);
 });
 
 app.listen(PORT);
